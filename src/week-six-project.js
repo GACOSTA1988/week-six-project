c7847bb73b3c9e3574d99728983e4e95e0d28f69
George <greg@gregs-MacBook-Pro.local>
2019-11-22 10:28:06 -0800

    ASYNC with inline API KEYTEST

--- src/week-six-project.js
+++ src/week-six-project.js
@@ -3,7 +3,7 @@ export class DoctorLocation {
     try {
 
       // THIS WILL CALL API WITH LOCATION
-      let response =  await fetch(``);
+      let response =  await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=3ce28e6513aaf0d2fe1c11e1e05072e0`);
 
       let jsonifiedResponse = await response.json();
       return jsonifiedResponse;
