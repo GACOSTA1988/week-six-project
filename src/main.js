1355788f5cc8f40775e17e20375a383abf1a1b52
George <greg@host1841691164.direcway.com>
2019-11-22 08:02:22 -0800

    add initial files

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// import { setIntervalAsync } from 'set-interval-async/dynamic';
+// import { clearIntervalAsync } from 'set-interval-async';
+// import $ from 'jquery';
+// import 'bootstrap';
+// import './styles.css';
+//
+// $(document).ready(function() {
+//   $('#').click(function() {
+//     const inputedNumber = $('#number').val();
+//
+//   });
+// });
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new UglifyJsPlugin({ sourceMap: true }),
+    new CleanWebpackPlugin(),
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+      title: 'tamagotchi',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 100000, // Convert images < 8kb to base64 strings
+            name: 'img/[hash]-[name].[ext]'
+          }
+        }]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/,
+          /spec/
+        ],
+        loader: "eslint-loader"
+      }
+    ]
+  }
+};
