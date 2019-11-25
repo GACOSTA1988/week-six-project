eb08932cfc98bf327d63f44c37878083762a3a4a
George <greg@gregs-MBP.home>
2019-11-24 16:54:41 -0800

    add function to retrieve is doc is accepting patients, add to display on dom func

--- src/main.js
+++ src/main.js
@@ -57,6 +57,14 @@ $(document).ready(function() {
         return "No phone number for this practice";
       }
     };
+    // THIS WILL CHECK IF DOC IS ACCEPTING NEW PATIENTS
+    const printOpenToPatients = (practicesObject) => {
+      if (practicesObject && practicesObject.accepts_new_patients === true) {
+        return `The doctor is accepting new patients`;
+      } else {
+        return "Sorry but the doctor is not accepting new patients";
+      }
+    };
     // THIS WILL DISPLAY API FUNCTION RETURN ON (DOM)
     function getElementsNameBase(responseName) {
       $("#showByNameSection").text(`The following doctors names best match ${inputedName}:`);
@@ -64,22 +72,27 @@ $(document).ready(function() {
       $("#docByName1").text(`${responseName.data[0].profile.first_name} ${responseName.data[0].profile.middle_name} ${responseName.data[0].profile.last_name}, ${responseName.data[0].profile.title}`);
       $("#docPhone1").text(printPhoneNumber(responseName.data[0].practices[0].phones[0]));
       $("#docWebSite1").text(printWebsiteUrl(responseName.data[0].practices[0]));
+      $("#docOpenToPatients1").text(printOpenToPatients(responseName.data[0].practices[0]));
 
       $("#docByName2").text(`${responseName.data[1].profile.first_name} ${responseName.data[1].profile.middle_name} ${responseName.data[1].profile.last_name}, ${responseName.data[1].profile.title}`);
       $("#docPhone2").text(printPhoneNumber(responseName.data[1].practices[0].phones[0]));
       $("#docWebSite2").text(printWebsiteUrl(responseName.data[1].practices[0]));
+      $("#docOpenToPatients2").text(printOpenToPatients(responseName.data[1].practices[0]));
 
       $("#docByName3").text(`${responseName.data[2].profile.first_name} ${responseName.data[2].profile.middle_name} ${responseName.data[2].profile.last_name}, ${responseName.data[2].profile.title}`);
       $("#docPhone3").text(printPhoneNumber(responseName.data[2].practices[0].phones[0]));
       $("#docWebSite3").text(printWebsiteUrl(responseName.data[2].practices[0]));
+      $("#docOpenToPatients3").text(printOpenToPatients(responseName.data[2].practices[0]));
 
       $("#docByName4").text(`${responseName.data[3].profile.first_name} ${responseName.data[3].profile.middle_name} ${responseName.data[3].profile.last_name}, ${responseName.data[3].profile.title}`);
       $("#docPhone4").text(printPhoneNumber(responseName.data[3].practices[0].phones[0]));
       $("#docWebSite4").text(printWebsiteUrl(responseName.data[3].practices[0]));
+      $("#docOpenToPatients4").text(printOpenToPatients(responseName.data[3].practices[0]));
 
       $("#docByName5").text(`${responseName.data[4].profile.first_name} ${responseName.data[4].profile.middle_name} ${responseName.data[4].profile.last_name}, ${responseName.data[4].profile.title}`);
       $("#docPhone5").text(printPhoneNumber(responseName.data[4].practices[0].phones[0]));
       $("#docWebSite5").text(printWebsiteUrl(responseName.data[4].practices[0]));
+      $("#docOpenToPatients5").text(printOpenToPatients(responseName.data[4].practices[0]));
     }
   });
 });
