92570e4ee3c7ca2ea42ebcf236414c63c8607736
George <greg@gregs-MBP.home>
2019-11-24 17:57:40 -0800

    add target object keys to get doc by condition (display function)

--- src/main.js
+++ src/main.js
@@ -7,11 +7,36 @@ import 'bootstrap';
 import './styles.css';
 
 $(document).ready(function() {
+  // THIS WILL CATCH UNDEFINED WEBSITE RETURN
+  const printWebsiteUrl = (practicesObject) => {
+    if (practicesObject && practicesObject.website) {
+      return `Doctor Website: ${practicesObject.website}`;
+    } else {
+      return "No website for this practice";
+    }
+  };
+
+  // THIS WILL CATCH UNDEFINED NUMBER RESULTS
+  const printPhoneNumber = (practicesObject) => {
+    if (practicesObject && practicesObject.number) {
+      return `Doctor Phone Number: ${practicesObject.number}`;
+    } else {
+      return "No phone number for this practice";
+    }
+  };
+  // THIS WILL CHECK IF DOC IS ACCEPTING NEW PATIENTS
+  const printOpenToPatients = (practicesObject) => {
+    if (practicesObject && practicesObject.accepts_new_patients === true) {
+      return `The doctor is accepting new patients`;
+    } else {
+      return "Sorry but the doctor is not accepting new patients";
+    }
+  };
+  // THIS FUBCTION WILL SEARCH DOCS BY CONDTION
   $('#conInput').click(function(event) {
     event.preventDefault();
     const inputedCondition = $('#inputedCondition').val();
     const inputedName = $('#inputedName').val();
-
     // THIS WILL RUN API CALL
     (async () => {
       let newDoctorByCondition = new DoctorByCondition();
@@ -23,14 +48,40 @@ $(document).ready(function() {
     // THIS WILL DISPLAY API FUNCTION RETURN ON (DOM)
     function getElements(response) {
       $("#showConditionSection").text(`If you are experiencing ${inputedCondition} symptoms you can reach out the the following doctors:`);
+      // DOC 2 DETAILS
       $("#doc1").text(`${response.data[0].profile.first_name} ${response.data[0].profile.middle_name} ${response.data[0].profile.last_name}, ${response.data[0].profile.title}`);
+      $("#doc1Address").text(`Visit address is: ${response.data[0].practices[0].visit_address.street}, ${response.data[0].practices[0].visit_address.city} ${response.data[0].practices[0].visit_address.state}, ${response.data[0].practices[0].visit_address.zip}`);
+      $("#doc1Phone").text(printPhoneNumber(response.data[0].practices[0].phones[0]));
+      $("#doc1WebSite").text(printWebsiteUrl(response.data[0].practices[0]));
+      $("#doc1OpenToPatients").text(printOpenToPatients(response.data[0].practices[0]));
+      // DOC 2 DETAILS
       $("#doc2").text(`${response.data[1].profile.first_name} ${response.data[1].profile.middle_name} ${response.data[1].profile.last_name}, ${response.data[1].profile.title}`);
+      $("#doc2Address").text(`Visit address is: ${response.data[1].practices[0].visit_address.street}, ${response.data[1].practices[0].visit_address.city} ${response.data[1].practices[0].visit_address.state}, ${response.data[1].practices[0].visit_address.zip}`);
+      $("#doc2Phone").text(printPhoneNumber(response.data[1].practices[0].phones[0]));
+      $("#doc2WebSite").text(printWebsiteUrl(response.data[1].practices[0]));
+      $("#doc2OpenToPatients").text(printOpenToPatients(response.data[1].practices[0]));
+      // DOC 3 DETAILS
       $("#doc3").text(`${response.data[2].profile.first_name} ${response.data[2].profile.middle_name} ${response.data[2].profile.last_name}, ${response.data[2].profile.title}`);
+      $("#doc3Address").text(`Visit address is: ${response.data[2].practices[0].visit_address.street}, ${response.data[2].practices[0].visit_address.city} ${response.data[2].practices[0].visit_address.state}, ${response.data[2].practices[0].visit_address.zip}`);
+      $("#doc3Phone").text(printPhoneNumber(response.data[2].practices[0].phones[0]));
+      $("#doc3WebSite").text(printWebsiteUrl(response.data[2].practices[0]));
+      $("#doc3OpenToPatients").text(printOpenToPatients(response.data[2].practices[0]));
+      // DOC 4 DETAILS
       $("#doc4").text(`${response.data[3].profile.first_name} ${response.data[3].profile.middle_name} ${response.data[3].profile.last_name}, ${response.data[3].profile.title}`);
+      $("#doc4Address").text(`Visit address is: ${response.data[3].practices[0].visit_address.street}, ${response.data[3].practices[0].visit_address.city} ${response.data[3].practices[0].visit_address.state}, ${response.data[3].practices[0].visit_address.zip}`);
+      $("#doc4Phone").text(printPhoneNumber(response.data[3].practices[0].phones[0]));
+      $("#doc4WebSite").text(printWebsiteUrl(response.data[3].practices[0]));
+      $("#doc4OpenToPatients").text(printOpenToPatients(response.data[3].practices[0]));
+      // DOC 5 DETAILS
       $("#doc5").text(`${response.data[4].profile.first_name} ${response.data[4].profile.middle_name} ${response.data[4].profile.last_name}, ${response.data[4].profile.title}`);
+      $("#doc5Address").text(`Visit address is: ${response.data[4].practices[0].visit_address.street}, ${response.data[4].practices[0].visit_address.city} ${response.data[4].practices[0].visit_address.state}, ${response.data[4].practices[0].visit_address.zip}`);
+      $("#doc5Phone").text(printPhoneNumber(response.data[4].practices[0].phones[0]));
+      $("#doc5WebSite").text(printWebsiteUrl(response.data[4].practices[0]));
+      $("#doc5OpenToPatients").text(printOpenToPatients(response.data[4].practices[0]));
     }
 
   });
+
   $('#nameInput').click(function() {
     const inputedName = $('#inputedName').val();
 
@@ -40,31 +91,6 @@ $(document).ready(function() {
       console.log(responseName);
       getElementsNameBase(responseName);
     })();
-    // THIS WILL CATCH UNDEFINED WEBSITE RETURN
-    const printWebsiteUrl = (practicesObject) => {
-      if (practicesObject && practicesObject.website) {
-        return `Doctor Website: ${practicesObject.website}`;
-      } else {
-        return "No website for this practice";
-      }
-    };
-
-    // THIS WILL CATCH UNDEFINED NUMBER RESULTS
-    const printPhoneNumber = (practicesObject) => {
-      if (practicesObject && practicesObject.number) {
-        return `Doctor Phone Number: ${practicesObject.number}`;
-      } else {
-        return "No phone number for this practice";
-      }
-    };
-    // THIS WILL CHECK IF DOC IS ACCEPTING NEW PATIENTS
-    const printOpenToPatients = (practicesObject) => {
-      if (practicesObject && practicesObject.accepts_new_patients === true) {
-        return `The doctor is accepting new patients`;
-      } else {
-        return "Sorry but the doctor is not accepting new patients";
-      }
-    };
     // THIS WILL DISPLAY API FUNCTION RETURN ON (DOM)
     function getElementsNameBase(responseName) {
       $("#showByNameSection").text(`The following doctors names best match ${inputedName}:`);
